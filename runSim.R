fa95ca6a66a991bde3c69fbf8d59b63cd34404ac
YuriGassner <yuri.gassner@gmx.net>
2021-03-21 17:48:37 +0100

    yes

--- runSim.R
+++ runSim.R
@@ -1,14 +1,16 @@
 ## Clean up the workspace
 rm(list=ls(all=TRUE))
 
-
+library("SimDesign")
+library("lavaan")
 set.seed(400)
-
+source("init.R")
+source("simFunctions.R")
 
 ###--------------------------###
 #Calculate "True" FMI
 
-simData(parms)
+simData(parm)
 calcFMI
 
 
