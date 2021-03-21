e50f92c91315c743ad6160d2fbb5747fd36a1361
YuriGassner <yuri.gassner@gmx.net>
2021-03-21 12:46:24 +0100

    functions

---
+++ simFunctions.R
@@ -0,0 +1,103 @@
+##Libraries
+library("SimDesign")
+
+# Functions
+##-------------------------------------------------------------------------------------------------------------------##
+simData <- function(n, coefficients, covariance, r.sqrd)
+{
+  #Construct covariance matrix
+  nrpred <- length(coefficients) - 1
+  sigma <- matrix(covariance, nrpred, nrpred)
+  diag(sigma) <- 1.0
+  
+  #Generate data
+  X <- rmvnorm(n = n, mean = rep(0, nrpred), sigma = sigma)
+  
+  #Generate error termn
+  #if r.sqrd = 0, we need to fix this stuff.
+  
+  
+  beta <- coefficients[-1]
+  if(r.sqrd > 0)
+  {
+    var.model <- t(beta) %*% cov(X) %*% beta
+    var.residual <- (var.model/r.sqrd) - var.model
+    U = rnorm(n, mean = 0, sd = sqrt(var.residual))
+    #compute Y
+    Y <- coefficients[1] + X  %*% beta + U 
+  }
+  else
+  {
+    Y <- rnorm(n=500, mean=0, sd=1)
+  }
+  
+  
+  
+  data <- data.frame(X)
+  
+  #Add to data frame
+  data$Y <- as.vector(Y)
+  
+  #Return data
+  data
+}
+
+##-------------------------------------------------------------------------------------------------------------------##
+# data       - the data frame which should get missing observations
+# mechanism  - the mechanism of missing data, by default MCAR
+# percent    - the proportion of observations that should be set to missing (NA)
+# indices    - A vector of indices indicating which columns should contain missing values
+makeMissing <- function(data, 
+                        mechanism="MCAR", 
+                        pm, 
+                        preds, 
+                        snr=NULL)
+{
+  #MAR missing data mechanism
+  if(mechanism=="MAR")
+  {
+    out <- simLinearMissingness(pm       = pm,
+                                data     = data,
+                                snr      = snr,
+                                preds    = preds,
+                                type     = "high",
+                                optimize = FALSE)
+    
+    out
+  }
+  
+  #MCAR missing data mechanism
+  else if(mechanism=="MCAR")
+  {
+    r <- sample(1:nrow(data), nrow(data)*pm)
+    tmp <- rep(FALSE, nrow(data))
+    tmp[r] <- TRUE
+    r <- tmp
+    out <- list(r   = r)#,
+    #eta = eta2,
+    #auc = auc,
+    #snr = sd(eta) / sqrt(var(eta2) - var(eta)))
+    #return
+    out
+  }
+  else
+  {
+    stop("Undefined or unsupported missing data mechanism.")
+  }
+}
+
+
+###----------------------------------------------------------###
+
+
+calcFMI <- function (data)
+{
+  
+
+}
+
+
+###----------------------------------------------------------###
+
+
+impData <- function(data)
