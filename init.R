33696ac871133b7afc702643599e2be650075a78
YuriGassner <yuri.gassner@gmx.net>
2021-03-21 14:28:53 +0100

    change

--- init.R
+++ init.R
@@ -20,7 +20,13 @@ parm$mec    <- c("MCAR", "MAR")
 parm$pm     <- c(0.1, 0.25, 0.5, 0.75, 0.9)
 parm$m      <- c(5, 50, 100, 250, 500)
 parm$iter   <- c(1)
-parm$n      <- c(100)
+parm$n      <- 100
 parm$rsq    <- c(0.5)
 parm$cov    <- c(5)
 parm$snr    <- c(0.65) #Signal to Noise ratio
+parm$coef   <- list(c(0,1), # Intercept / Slope of Y1
+                    c(0,1), # Intercept / Slope of Y2
+                    c(0,1)) # Intercept / Slope of Y3
+
+
+
