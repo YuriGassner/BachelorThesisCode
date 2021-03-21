8f2f53b80c451955d8637d0d069a3e143e28807a
YuriGassner <yuri.gassner@gmx.net>
2021-03-21 12:46:12 +0100

    Init

--- init.R
+++ init.R
@@ -1,16 +1,26 @@
 #Parameters of the simulation:
-parameters <- list()
+parm <- list()
+
+
 
 #Variable
-M <- 10                                     # Number of iterations
-nvec <- seq(100,10000,100)                  # sample sizes
-r.squared <- c(.1, .33, .5, .66, .9)        # R-squared
-coeff_list <- list(c(1, .5),
-                   c(1, .5, 3.3),
-                   c(1, .5, 3.3, 2),
-                   c(1, .5, 3.3, 2, 1.5))   # Coefficients for the predictor variables, also number of predictors (length -1)
+#covariances <- c(0, 3.33, 10)       # Covariances
+#r.squared   <- c(0, .1, .5)         # R-square
+#mechanism   <- c("MCAR", "MAR")     # Missing data mechanism
+#snr         <- c(.65, .75)          # SNR
+#pm          <- c(0.1, 0.25, 0.5, 0.75, 0.9)
+#m           <- c(5, 50, 100, 250, 500)
+#iter        <- 1                    # iterations
 
 #Fixed
-parameters$covariance <- c(.1)                              #covariances
-
-
+#parameters$n    <- 100              #sample size
+#parameters$c    <- list(c(1, 1.5),  #Coefficients for study 1
+#                        c(0, 2))    #and 2 respectively
+parm$mec    <- c("MCAR", "MAR")
+parm$pm     <- c(0.1, 0.25, 0.5, 0.75, 0.9)
+parm$m      <- c(5, 50, 100, 250, 500)
+parm$iter   <- c(1)
+parm$n      <- c(100)
+parm$rsq    <- c(0.5)
+parm$cov    <- c(5)
+parm$snr    <- c(0.65) #Signal to Noise ratio
