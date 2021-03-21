9ce81e1fa603efd1a483de33ca07702a375542e9
YuriGassner <yuri.gassner@gmx.net>
2021-03-21 18:20:51 +0100

    infinity practice test

--- runSim.R
+++ runSim.R
@@ -7,7 +7,26 @@ set.seed(400)
 source("init.R")
 source("simFunctions.R")
 
-data <- simData(parm)
+data <- simData(parm, infinity = "no")
+datainfinite <- simData(parm, infinity = "yes")
+
+mean(data$X1)
+mean(data$X2)
+mean(data$X3)
+mean(data$Y)
+mean(datainfinite$X1)
+mean(datainfinite$X2)
+mean(datainfinite$X3)
+mean(datainfinite$Y)
+
+sd(data$X1)
+sd(data$X2)
+sd(data$X3)
+sd(data$Y)
+sd(datainfinite$X1)
+sd(datainfinite$X2)
+sd(datainfinite$X3)
+sd(datainfinite$Y)
 
 ###--------------------------###
 #Calculate "True" FMI
