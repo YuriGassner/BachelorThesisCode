0bba7a5e19f8c6e7990866c70c58017ee21b7989
YuriGassner <yuri.gassner@gmx.net>
2021-03-21 14:29:12 +0100

    change

--- simFunctions.R
+++ simFunctions.R
@@ -3,26 +3,26 @@ library("SimDesign")
 
 # Functions
 ##-------------------------------------------------------------------------------------------------------------------##
-simData <- function(n, coefficients, covariance, r.sqrd)
+simData <- function (parm$n, parm$coef, parm$cov, parm$rsq)
 {
   #Construct covariance matrix
-  nrpred <- length(coefficients) - 1
-  sigma <- matrix(covariance, nrpred, nrpred)
+  nPred <- length(parm$coef) - 1
+  sigma <- matrix(parm$cov, parm$n, parm$n)
   diag(sigma) <- 1.0
   
   #Generate data
-  X <- rmvnorm(n = n, mean = rep(0, nrpred), sigma = sigma)
+  X <- rmvnorm(n = parm$n, mean = rep(0, nPred), sigma = sigma)
   
   #Generate error termn
   #if r.sqrd = 0, we need to fix this stuff.
   
   
   beta <- coefficients[-1]
-  if(r.sqrd > 0)
+  if(parm$rsq > 0)
   {
     var.model <- t(beta) %*% cov(X) %*% beta
-    var.residual <- (var.model/r.sqrd) - var.model
-    U = rnorm(n, mean = 0, sd = sqrt(var.residual))
+    var.residual <- (var.model/parm$rsq) - var.model
+    U = rnorm(parm$n, mean = 0, sd = sqrt(var.residual))
     #compute Y
     Y <- coefficients[1] + X  %*% beta + U 
   }
@@ -101,3 +101,63 @@ calcFMI <- function (data)
 
 
 impData <- function(data)
+
+
+    
+###--------------------------------------------------------------------------###
+  
+  ## Simulate a nonresponse vector via a linear probability model:
+  simLinearMissingness <- function(pm,
+                                   data,
+                                   auc      = NULL,
+                                   snr      = NULL,
+                                   optimize = TRUE,
+                                   preds    = colnames(data),
+                                   type     = "high",
+                                   beta     = rep(1.0, length(preds)),
+                                   stdData  = TRUE,
+                                   ...)
+  {
+    if(is.null(snr) & is.null(auc))
+      stop("You must define a value of either 'snr' or 'auc'")
+    
+    if(optimize & !type %in% c("high", "low"))
+      stop("'type' must be either 'high' or 'low'")
+    
+    ## Standardize the missing data predictors:
+    if(stdData) data <- scale(data)
+    
+    ## Define the (centered) linear predictor:
+    eta <- as.numeric(as.matrix(data) %*% matrix(beta))
+    
+    ## Find the optimal proportion of noise:
+    if(optimize) {
+      noise    <- rnorm(length(eta), 0, sd(eta))
+      noiseFit <- .optNoise(auc      = auc,
+                            eta      = eta,
+                            noise    = noise,
+                            type     = type,
+                            pm       = pm,
+                            logistic = FALSE,
+                            ...)
+      
+      ## Add noise to the linear predictor:
+      eta2 <- eta + (noiseFit$minimum * noise)
+    }
+    else
+      ## Define the noisy linear predictor in terms of the specified SNR:
+      eta2 <- eta + (1 / snr) * rnorm(length(eta), 0, sd(eta))
+    
+    r <- .linProbMissingness(eta = eta2, pm = pm, type = type)
+    
+    ## Compute the achieved AUC:
+    auc <- ifelse(type %in% c("high", "low"),
+                  as.numeric(auc(roc(r, eta, quiet = TRUE, ...))),
+                  NA)
+    
+    list(r   = r,
+         eta = eta2,
+         auc = auc,
+         snr = sd(eta) / sqrt(var(eta2) - var(eta))
+    )
+  }
