119d2d0229433441494cc140cfaa79d4fe7eb78b
YuriGassner <yuri.gassner@gmx.net>
2021-04-09 21:35:14 +0200

    yes

---
+++ analysis.R
@@ -0,0 +1,9 @@
+###-----------------------------------------------------------------###
+# Analysis
+# Read in CSV files
+
+# Average FMI estimate per condition
+
+# Calculate SE and CI of FMI estimates per condition
+
+# Potentially compare FMI estimations with true values
