81eb262cd6cb971a8a98d1268f7a2f5cbcdbbb7b
YuriGassner <yuri.gassner@gmx.net>
2021-04-09 17:24:39 +0200

    test

--- runSim.R
+++ runSim.R
@@ -1,53 +1,97 @@
 ## Clean up the workspace
+
 rm(list=ls(all=TRUE))
 
+
+## Libraries, Sources & Seed
+
 library("SimDesign")
+library("mice")
 library("lavaan")
-set.seed(400)
+library("semTools")
+
 source("init.R")
 source("simFunctions.R")
+source("simMissingness.R")
 
-# data <- simData(parm, infinity = "no")
-# datainfinite <- simData(parm, infinity = "yes")
-# 
-# mean(data$X1)
-# mean(data$X2)
-# mean(data$X3)
-# mean(data$Y)
-# mean(datainfinite$X1)
-# mean(datainfinite$X2)
-# mean(datainfinite$X3)
-# mean(datainfinite$Y)
-# 
-# sd(data$X1)
-# sd(data$X2)
-# sd(data$X3)
-# sd(data$Y)
-# sd(datainfinite$X1)
-# sd(datainfinite$X2)
-# sd(datainfinite$X3)
-# sd(datainfinite$Y)
-
-#FMI Test
-dataFinite <- simData(parm, infinity = "no")
-fmiFinite <- calcFMI(data = data, infinity = "no")
-
-###--------------------------###
-#Calculate "True" FMI
-
-dataInfinite <- simData(parm, infinity = "yes")
-fmiInfinite <- calcFMI(data = datainfinite, infinity = "yes")
- 
-
-###----------------------------------------###
+set.seed(541491)
 
 
-###--------------------------###
-#Run Simulation
+## Storing the data as a list since the output of the analyses are multiple matrices
+## Conditions will be sorted 1:20
 
-for (i in parm$iter)
+store <- vector("list", length = parm$Ncond)
+
+
+# store[[7]] <- n5
+
+# Name them
+
+# Reference them each, e.g. in conditions varying pm  from 5:10 
+
+
+
+
+## Store resulting data frame of each iteration
+
+store_i <- vector("list", parm$iter)
+
+###-----------------------------------------------------------------###
+# Simulation Loop
+
+for (a in 1:parm$iter)
 {
-  table <- list() #Create a data table for each iteration containing all relevant information
-
-  simData(parms)
+  #Generate Data
+  
+  data <- try(simData(parm = parm,
+                      infinity = "no"))
+  
+  #Generate Missing Values
+  for (b in 1:length(parm$mec))  #Loop for Mechanism
+  {
+    for (c in 1:length(parm$pm))  #Loop for Percent-Missing
+    {
+      #Create MissingnessMatrix with TRUE/FALSE values
+      MissingMatrix <- try(makeMissing(data = data,
+                                mechanism = parm$mec[[b]],
+                                pm = parm$pm[[c]],
+                                preds = parm$pred,
+                                snr = NULL
+                                ))
+      #Impose Missingness
+      MissingDf <- data
+      MissingDf[MissingMatrix$r,1] <- NA 
+      
+      #Impute Missing Values
+      for (d in 1:length(parm$m))
+      {
+        ImpData <- try(mice(data = MissingDf,
+                            m = parm$m[[d]],
+                            method = "norm",
+                            print = FALSE
+                            ))
+        
+        #Save a list of imputed data sets
+        ImpListDf <- try(complete(data = ImpData,
+                                  action = "all"
+                                  ))
+        
+        #Calculate FMIs 
+        fmi <- try(fmi(data = ImpListDf,
+                       method = "sat",
+                       fewImps = TRUE
+                       ))
+        
+        #Save FMIs to list
+        
+      }
+    }
+  
+  }
+  
+  #Save list to Location
+  X=X
 }
+
+# End Simulation
+###-----------------------------------------------------------------###
