a413f505741f87c7c70f4aa54efc5c0b8dcbfee3
YuriGassner <yuri.gassner@gmx.net>
2021-04-21 23:26:41 +0200

    Need, trueFMI test + counter fix + cbind condition matrix to output

--- runSim.R
+++ runSim.R
@@ -17,8 +17,10 @@ source("simMissingness.R")
 set.seed(541491)
 
 
-
-
+#### STILL NEEDS: 
+# CBIND the condition matrix on the output
+# Fix counter for doRep 
+# Test trueFMI calculation
 
 
 ##-------------------------------------------------------------------------------------------------------------------##
@@ -48,6 +50,20 @@ for(i in 1:10)
   time[[i]] <- Sys.time()
 }
 
+time0_1 <- time[[1]] - start_time
+time1_2 <- time[[2]] - time[[1]]
+time2_3 <- time[[3]] - time[[2]]
+time3_4 <- time[[4]] - time[[3]]
+time4_5 <- time[[5]] - time[[4]]
+time5_6 <- time[[6]] - time[[5]]
+time6_7 <- time[[7]] - time[[6]]
+time7_8 <- time[[8]] - time[[7]]
+time8_9 <- time[[9]] - time[[8]]
+time9_10 <- time[[10]] - time[[9]]
+total_time <- time[[10]] - start_time
+
+mean1_10 <- mean(time0_1, time1_2, time2_3, time3_4, time4_5, time5_6, time6_7, time7_8, time8_9, time9_10)
+sd1_10 <- sd(time0_1, time1_2, time2_3, time3_4, time4_5, time5_6, time6_7, time7_8, time8_9, time9_10)
 
 
 mean(time)
@@ -64,7 +80,7 @@ time
 #21th of April - doRepMultipleData - 11.62618min ; doRep function but generating data each time
 #21th of April - both functions - 10.8min ; no other open programs aside from Terminal, doRep slightly faster, slightly
 #                                           different results -> use only one simulated file per iteration
-
+#21th of April - doRep 10iterations - slightly below 11min ; COUNTER DOES NOT WORK, OVERWRITES FILE 1
 
 
 #Time needed for 1000 iterations on 4 cores (slightly more as not all CPUs can be utilised fully simultaneously)
