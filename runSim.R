e725df910c4f040a380365a600ed69cc4ca5c513
YuriGassner <yuri.gassner@gmx.net>
2021-04-21 19:54:53 +0200

    still needs maincode but all functions and init.R are ready

--- runSim.R
+++ runSim.R
@@ -16,74 +16,15 @@ source("simMissingness.R")
 
 set.seed(541491)
 
-##################################################
-
-for (i in 1 : nrow(conds))
-{
-  
-  #Save current values of the varying values
-  parm$m <- conds[i, "m"]
-  parm$mec <- conds[i, "mec"]
-  parm$pm <- conds[i, "pm"]
-  
-  
-  #Simulate Data
-  data <- try(simData(parm = parm,))
-  
-  
-  #Create Missingdata Matrix
-  missingMatrix <- try(makeMissing(data = data,
-                                   mechanism = parm$mec,
-                                   pm = parm$pm,
-                                   preds = parm$Vecpred,
-                                   snr = NULL
-  ))
-  
-  
-  # #Impose Missingness
-  # missingdf <- data
-  # missingdf[missingMatrix$r , 1] <- NA
-  
-  
-  #Impute Missing Values
-  impData <- try(mice(data = missingdf,
-                      m = parm$m,
-                      method = "norm",
-                      print = FALSE
-  ))
-  
-  
-  #Save a List of Imputed Data Sets
-  impListdf <- try(complete(data = impData,
-                            action = "all"
-  ))
-  
-  
-  #Compute FMIs 
-  fmi <- try(fmi(data = impListdf,
-                 method = "sat",
-                 fewImps = TRUE
-  ))
-  
-  
-  #Save FMIs to List
-  store[[i]] <- fmi
-
-}
-
-
-#Write List to disc
-saveRDS(store, 
-        file = paste0("results/data_it_",a,".rds"))
 
 
 
 
 
-
-#######
-# Notes: Rewrite Loopstructure, Create "Condition Matrix" -> flattens out the loops
-#        Label the conditions somehow?? 
+##-------------------------------------------------------------------------------------------------------------------##
+#Notes
+#  Rewrite Loopstructure, Create "Condition Matrix" -> flattens out the loops
+#  Label the conditions somehow?? 
 #        
 ######
 #  expand.grid function (vectors that are named. e.g. imp= imp) creates a data frame
@@ -91,104 +32,46 @@ saveRDS(store,
 #  take the values from the condition matrix x <- conds[i, "rsq"]
 #  cbind the condition matrix on the output
 ######
-#  FMI with large N, poke holes into it and run semTools function
+#  FMI with large N, poke holes into it and run semTools function on empty data set
 #  Create 10 mega datasets for Mechanism X PM conditions 
-#  
-######
-# class(store)
+#  Does the a + 1 counter still work for parallelisation
+##-------------------------------------------------------------------------------------------------------------------##
+#Main Simulation
 
-# store[[7]] <- n5
-
-# Name them
-
-# Reference them each, e.g. in conditions varying pm  from 5:10 
-
-
-
-
-## Store resulting data frame of each iteration
-
-store_i <- vector("list", parm$iter)
-
-###-----------------------------------------------------------------###
-# Simulation Loop
 start_time <- Sys.time()
 
-for (a in 1:parm$iter) ### KML: See comment in Bastian's code about main-style
-                       ### function.
+time <- vector("list", length = 10)
+#Currently does one rep
+for(i in 1:10)
 {
-  
-  #Generate Data
-  data <- try(simData(parm = parm,
-                      infinity = "no"))
-  
-  #Set/Reset counter
-  i = 1
-  
-  #Generate Missing Values
-  for (b in 1:length(parm$mec))  #Loop for Mechanism mec
-  {
-    for (c in 1:length(parm$pm))  #Loop for Percent-Missing pm
-    {
-      #Create MissingnessMatrix with TRUE/FALSE values
-      MissingMatrix <- try(makeMissing(data = data,
-                                mechanism = parm$mec[[b]],
-                                pm = parm$pm[[c]],
-                                preds = parm$Vecpred,
-                                snr = NULL
-                                ))
-      #Impose Missingness
-      MissingDf <- data
-      MissingDf[MissingMatrix$r,1] <- NA 
-      
-      #Impute Missing Values
-      for (d in 1:length(parm$m))  #Loop for Missing Values m
-      {
-          
-        #Impute Missing Data Sets
-        ImpData <- try(mice(data = MissingDf,
-                            m = parm$m[[d]],
-                            method = "norm",
-                            print = FALSE
-                            ))
-        
-        #Save a List of Imputed Data Sets
-        ImpListDf <- try(complete(data = ImpData,
-                                  action = "all"
-                                  ))
-
-                             
-        #Calculate FMIs with semTools as 'lavaan' only does ML fmi
-        fmi <- try(fmi(data = ImpListDf,
-                       method = "sat",
-                       fewImps = TRUE
-                       ))
-        
-        #Save FMIs to list
-        store[[i]] <- fmi
-        i <- i + 1
-      }
-    }
-  
-  }
-  
-  #Save list to Location
-    store_i[[a]] <- store
-
-    ### KML: Save your output as RDS files.
-  write.table(data.frame(store), 
-              file     = paste0("results/data_iteration_",a,".csv")) ## SAVE AS RDS
-  
+  doRep(conds, parm)
+  time[[i]] <- Sys.time()
 }
+
+
+
+mean(time)
+time
 
 end_time <- Sys.time()
 
-# Current time per Iteration ~10min30sec
+time <- end_time - start_time
+time 
+#Current best times for one iteration on one core:
+
+#21th of April - doRep - 13min ; new doRep function, same time as with multiple for loops
+#21th of April - doRep - 11.099min ; simulating the data only once, saved about 2min per iteration
+#21th of April - doRepMultipleData - 11.62618min ; doRep function but generating data each time
+#21th of April - both functions - 10.8min ; no other open programs aside from Terminal, doRep slightly faster, slightly
+#                                           different results -> use only one simulated file per iteration
+
+
+
+#Time needed for 1000 iterations on 4 cores (slightly more as not all CPUs can be utilised fully simultaneously)
+total_time <- (time*1000)/4
+total_time
 
 # End Simulation
-###-----------------------------------------------------------------###
-# Analysis
-# Read in CSV files
-# Average FMI estimate per condition
-# Calculate SE and CI of FMI estimates per condition
+##-------------------------------------------------------------------------------------------------------------------##
+
 
