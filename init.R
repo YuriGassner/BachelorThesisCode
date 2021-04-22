8d473277479b1fedb8d9507ffc7c8d3269d26be8
YuriGassner <yuri.gassner@gmx.net>
2021-04-22 17:53:29 +0200

    minor updates to fit the code

--- init.R
+++ init.R
@@ -14,12 +14,13 @@ parm$snr        <- c(1)                           #Signal to Noise ratio
 parm$pred       <- c(2)                           #Number of Predictors: X1, X2
 parm$Vecpred    <- c(1,1)                         #Vector of N pred, otherwise simLinearMissingness doesn't work
 parm$Ncond      <- c(50)                          #Number of conditions: mec x pm x m
+var <- list()
 
 mec             <- c("MCAR", "MAR")               #Missingness mechanism
-pm              <- c(0.1, 0.25, 0.5, 0.75, 0.9)   #Percent Missing
-m               <- c(5, 50, 100, 250, 500)        #Number of imputations
+pm              <- c(0.9, 0.75, 0.5, 0.25, 0.1)   #Percent Missing
+m               <- c(500, 250, 100, 50, 5)        #Number of imputations
 
-conds <- expand.grid(mec = mec, pm = pm, m = m)   #Condition Matrix
+conds <- expand.grid(m = m, pm = pm, mec = mec)   #Condition Matrix, specified so that m changes first while other par remain constant
 
 store <- vector("list", length = nrow(conds))     #List of matrices for temporary storage of each iteration
 storage <- vector("list", length = nrow(conds))   #List of matrices for temporary storage of the trueFMI values
