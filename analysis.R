ee25fffe2517dce51b785281f2669307fbfb2fad
YuriGassner <yuri.gassner@gmx.net>
2021-04-27 13:37:22 +0200

    yup

--- analysis.R
+++ analysis.R
@@ -1,9 +1,18 @@
 ###-----------------------------------------------------------------###
 # Analysis
-# Read in CSV files
+# Read in RDS files
+
+hallotest <- cbind(conds, store)
+hallo2 <- cbind(conds2, data_it_1)#Creates the correct matrix with cond added to the matrix on column 1 to 3
+
+hallo2test <- hallo2[19,]
+hallo1test <- hallotest[4,]
+# Exclude but mention failed iterations, adapt analysis accordingly
 
 # Average FMI estimate per condition
 
 # Calculate SE and CI of FMI estimates per condition
 
 # Potentially compare FMI estimations with true values
+
+# Make Pretty Graphs
